b1b301382e328905a4dd98f9fa561fb93cf613c9
Lesnek <milan.lesnek@kiwi.com>
2022-12-02 18:29:01 +0100

    chore(rename): Rename to solutions also for python

--- python/day1/solution.py
+++ python/day1/solution.py
@@ -2,12 +2,12 @@ class Elf:
     calories: int = 0
 
 
-def day1(data: list[str]) -> int:
+def solution1(data: list[str]) -> int:
     elves = get_elves_with_calories(data)
     return max(elf.calories for elf in elves)
 
 
-def day1_2(data: list[str]) -> int:
+def solution2(data: list[str]) -> int:
     elves = get_elves_with_calories(data)
     return get_sum_of_top_3(elves)
 
@@ -35,5 +35,5 @@ def get_elves_with_calories(data: list[str]) -> list[Elf]:
 
 if __name__ == "__main__":
     input_1 = parse_input("../../inputs/day1.txt")
-    print(f"Maximum calories for one elf is: {day1(input_1)}")
-    print(f"Sum of top three calories per elf is: {day1_2(input_1)}")
+    print(f"Maximum calories for one elf is: {solution1(input_1)}")
+    print(f"Sum of top three calories per elf is: {solution2(input_1)}")
--- python/tests/test_day1.py
+++ python/tests/test_day1.py
@@ -1,20 +1,20 @@
 import unittest
 
-from python.day1.calories import day1, day1_2
+from python.day1.solution import solution1, solution2
 
 
 class Test(unittest.TestCase):
 
     def test_day1(self):
-        self.assertEqual(day1(["1", "2", "3"]), 6)
-        self.assertEqual(day1([]), 0)
-        self.assertEqual(day1(["1", "\n", "2"]), 2)
+        self.assertEqual(solution1(["1", "2", "3"]), 6)
+        self.assertEqual(solution1([]), 0)
+        self.assertEqual(solution1(["1", "\n", "2"]), 2)
 
     def test_day1_2(self):
-        self.assertEqual(day1_2(["1", "2", "3"]), 6)
-        self.assertEqual(day1_2([]), 0)
-        self.assertEqual(day1_2(["1", "\n", "2"]), 3)
-        self.assertEqual(day1_2(["1", "\n", "2", "\n", "3", "\n", "4"]), 9)
+        self.assertEqual(solution2(["1", "2", "3"]), 6)
+        self.assertEqual(solution2([]), 0)
+        self.assertEqual(solution2(["1", "\n", "2"]), 3)
+        self.assertEqual(solution2(["1", "\n", "2", "\n", "3", "\n", "4"]), 9)
 
 
 if __name__ == '__main__':
