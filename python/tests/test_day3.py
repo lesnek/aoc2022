2f6de7243f95eff39aaece9ab50fdc0f89e06f56
Lesnek <milan.lesnek@kiwi.com>
2022-12-03 09:52:32 +0100

    chore(black): format all python

--- python/tests/test_day3.py
+++ python/tests/test_day3.py
@@ -4,7 +4,6 @@ from python.day3.solution import solution1, solution2
 
 
 class Test(unittest.TestCase):
-
     def test_day3(self):
         self.assertEqual(solution1(["aa", "BB"]), 1 + 28)  # a + B
 
@@ -12,5 +11,5 @@ class Test(unittest.TestCase):
         self.assertEqual(solution2(["xdeA", "fsaA", "lkjA"]), 27)  # A = 27
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
