6dab3dc77480738622df9858f217acdea5763f4e
Lesnek <milan.lesnek@kiwi.com>
2022-12-09 08:27:51 +0100

    feat(day9): python solution

--- python/tests/test_day9.py
+++ python/tests/test_day9.py
@@ -1,10 +1,10 @@
 import unittest
 
-from python.day9.solution import solution1
+from python.day9.solution import solution1, solution2
 
 
 class Test(unittest.TestCase):
-    inputs = [
+    inputs1 = [
         "R 4",
         "U 4",
         "L 3",
@@ -14,9 +14,22 @@ class Test(unittest.TestCase):
         "L 5",
         "R 2",
     ]
+    inputs2 = [
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20",
+    ]
 
     def test_sol1(self):
-        self.assertEqual(solution1(self.inputs), 13)
+        self.assertEqual(solution1(self.inputs1), 13)
+
+    def test_sol2(self):
+        self.assertEqual(solution2(self.inputs2), 36)
 
 
 if __name__ == "__main__":
